e1e59adcd1173d8fdd84a0bc6b0bcb9cb8955c3b
checheben <89188446@qq.com>
2016-05-25 17:28:53 +0800

    add the file

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,13 @@
 
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+	var arr = [];
+	for(var i=0;i<collection_a.length;i++){
+		var index = object_b.value.indexOf(collection_a[i].key);
+		if(index>=0){
+			arr.push(collection_a[i].key); 
+		}
+	}
+	return arr;
 }
 
 module.exports = collect_same_elements;
